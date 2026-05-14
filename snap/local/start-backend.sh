a137c1fd85f413aac842ecfc6b8e3ffcd91efe4b
tphan025 <trung.thanh.phan@canonical.com>
2026-05-14 09:59:46 +0200

    update runuser

--- snap/local/start-backend.sh
+++ snap/local/start-backend.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PYTHONHOME="$SNAP/usr"
 export PYTHONPATH="$SNAP/lib/python3.12/site-packages:$SNAP/usr/lib/python3.12"
+export PATH="$SNAP/bin:$SNAP/usr/bin:/snap/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 cd "$SNAP"
-exec "$SNAP/bin/python3" "$SNAP/bin/flask" --app app run --host 0.0.0.0 --port 5000
+exec su -s /bin/sh ubuntu -c "PYTHONHOME='$PYTHONHOME' PYTHONPATH='$PYTHONPATH' PATH='$PATH' exec '$SNAP/bin/python3' '$SNAP/bin/flask' --app app run --host 0.0.0.0 --port 5000"
